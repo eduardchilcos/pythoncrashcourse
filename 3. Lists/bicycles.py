a781bf75b1ce3f68c6a31e8eeafd98ad0cd3beb7
eduardchilcos <eduard.chilcos@gmail.com>
2025-05-26 20:22:14 +0100

    reorganising folders

---
+++ 3. Lists/bicycles.py
@@ -0,0 +1,3 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialised']
+message = f"My first bycicle was a {bicycles[1].title()}."
+print(message)
---
+++ 3. Lists/friend_names.py
@@ -0,0 +1,5 @@
+names = ['Vlad', 'Gabriel', 'Mihai']
+message = f"Lui {name[0]} ii place sa fie stoic."
+print(names[0])
+print(names[1])
+print(names[2])
