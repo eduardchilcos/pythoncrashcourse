aa6102923c1e4316acfb66ac1e37eef13d78df5d
eduardchilcos <eduard.chilcos@gmail.com>
2025-05-26 20:58:27 +0100

    playing around with lists

--- 3. Lists/friend_names.py
+++ 3. Lists/friend_names.py
@@ -1,5 +1,13 @@
 names = ['Vlad', 'Gabriel', 'Mihai']
-message = f"Lui {name[0]} ii place sa fie stoic."
-print(names[0])
-print(names[1])
-print(names[2])
+message_one = f"Lui {names[0]} ii place sa fie stoic."
+message_two = f"Lui {names[1]} ii place sa fie stoic."
+message_three = f"Lui {names[2]} ii place shaorma."
+print(message_one, message_two, message_three)
+
+modes_of_transport = ['motorcycle', 'bus', 'bike', 'rickshaw', 'airplane', 'train']
+print(f"I would like to own a Honda Fury {modes_of_transport[0]} one day.")
+print(f"I live in London and I never take the {modes_of_transport[1]} because the traffic is too busy.")
+print(f"Back when I used to live in Oxford, I would love to ride the {modes_of_transport[2]} to the gym and back.")
+print(f"One day I hope to travel to Asia and experience a ride on a {modes_of_transport[3]}.")
+print(f"The worst part about travelling by {modes_of_transport[4]} is all the admin work involved with getting on and off the {modes_of_transport[4]}.")
+print(f"I love travelling by {modes_of_transport[5]} as it's extremely convenient when it runs on time.")
