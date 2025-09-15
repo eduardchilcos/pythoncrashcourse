96e80388a1e3f26f83ddd2453dca6e173149504e
eduardchilcos <eduard.chilcos@gmail.com>
2025-09-15 21:31:19 +0100

    we are so back

--- 4. Working with lists/pizzas.py
+++ 4. Working with lists/pizzas.py
@@ -3,3 +3,16 @@ for pizza in pizzas:
     print(f"{pizza.title()} has always been my favourite.")
 
 print("\nI always think\n that words cannot express\n how much I REALLY LOVE PIZZA!!!")
+
+friend_pizzas = pizzas[:]
+
+pizzas.append('capriciosa')
+friend_pizzas.append('hawaiian')
+
+print("My favourite pizzas are:")
+for pizza in pizzas:
+    print(pizza)
+
+print("My friend's favourite pizzas are:")
+for pizza in friend_pizzas:
+    print(pizza)
--- 4. Working with lists/players.py
+++ 4. Working with lists/players.py
@@ -1,3 +1,18 @@
 players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print(players[0:3])
+# print(players[-3:])
+
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
+
+
+print("The first three items in the list are:")
 print(players[0:3])
+
+print("Three items from the middle of the list are:")
+print(players[1:4])
+
+
+print("Three last three items in the list are:")
 print(players[-3:])
