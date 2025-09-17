a2391d1b0e050b13700953fa7f0e7eb1b9c3640f
eduardchilcos <eduard.chilcos@gmail.com>
2025-09-17 21:00:54 +0100

    another day another challenge

---
+++ 4. Working with lists/menu.py
@@ -0,0 +1,7 @@
+foods = ('fries', 'wings', 'burger', 'strips', 'pizza')
+for food in foods:
+    print(food)
+
+foods = ('broccoli', 'wings', 'burger', 'strips', 'pizza')
+for food in foods:
+    print(food)    
---
+++ 5. If Statements/cars.py
@@ -0,0 +1,38 @@
+# cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+# for car in cars:
+#    if car == 'bmw':
+#        print(car.upper())
+#    else:
+#        print(car.title())
+
+
+car = 'dacia'
+print("Is car == 'dacia'? I predict True.")
+print(car == 'dacia')
+
+print("\nIs car == 'renault'? I predict False.")
+print(car == 'renault')
+
+
+car = 'tesla'
+print("Is car == 'tesla'? I predict True.")
+print(car == 'tesla')
+
+print("\nIs car == 'ford'? I predict False.")
+print(car == 'ford')
+
+fruit = 'peach'
+print("Is fruit == 'peach'? I predict True")
+print(fruit == 'peach')
+
+print("\nIs fruit == 'apple'? I predict False.")
+print(fruit == 'apple')
+
+fruit = 'watermelon'
+print("Is fruit == 'watermelon'? I predict True.")
+print(fruit == 'watermelon')
+
+
+print("\nIs fruit == 'apple'? I predict False")
+print(fruit == 'apple')
