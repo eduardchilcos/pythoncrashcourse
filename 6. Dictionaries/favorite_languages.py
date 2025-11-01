56fe74a895b7d0a25bd73f9ab9f98107049ed979
Eduard <eduard.chilcos@gmail.com>
2025-11-01 23:12:43 +0000

    dictionaries in dictionaries

--- 6. Dictionaries/favorite_languages.py
+++ 6. Dictionaries/favorite_languages.py
@@ -1,9 +1,9 @@
-favourite_languages = {
+'''favourite_languages = {
     'jen': 'python',
     'sarah': 'c',
     'edward': 'rust',
     'phil': 'python',
-    }
+    }'''
 
 '''language = favourite_languages['sarah'].title()
 print(f"Sarah's favourite language is {language}.")
@@ -25,6 +25,31 @@ for name in favourite_languages.keys():
 '''for name in sorted(favourite_languages.keys()):
     print(f"{name.title()}, thank you for taking the poll.")'''
 
-print("The following languages have been mentioned:")
+'''print("The following languages have been mentioned:")
 for language in set(favourite_languages.values()):
-    print(language.title())
+    print(language.title())'''
+
+'''people = ['marcus', 'andrew', 'natalie', 'sophie', 'lucy', 'sarah', 'edward']
+
+for name in people:
+    if name in favourite_languages.keys():
+        print(f"\nThanks for taking the poll, {name.title()}.")
+    else:
+        print(f"\nYou've yet to take the poll, {name.title()}. Please do so.")'''
+
+favourite_languages = {
+    'jen': ['python', 'rust'],
+    'sarah': ['c'],
+    'edward': ['rust', 'go'],
+    'phil': ['python', 'haskell'],
+    }
+for name, languages in favourite_languages.items():
+    if len(languages) > 1:
+        print(f"\n{name.title()}'s favourite languages are:")
+        for language in languages:
+            print(f"\t{language.title()}")
+    elif len(languages) == 1:
+        favourite_language = languages[0]  # Explicitly extract the single language
+        print(f"\n{name.title()}'s favourite language is {favourite_language.title()}.")
+    else:
+        print(f"\n{name.title()} has not specified a favourite language.")
